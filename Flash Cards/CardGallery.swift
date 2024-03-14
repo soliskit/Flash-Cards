a9f434f78e06fd927acdb9c1622f688cb08c7733
David Solis <doze.inept.0n@icloud.com>
2024-03-14 16:11:55 -0700

    create vertical grid of cards

---
+++ Flash Cards/CardGallery.swift
@@ -0,0 +1,38 @@
+//
+//  CardGallery.swift
+//  Flash Cards
+//
+//  Created by David Solis on 3/14/24.
+//
+
+import SwiftUI
+
+struct CardGallery: View {
+    var cards: [Card]
+    let selectCard: (Card) -> Void
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 200))], spacing: 20) {
+                
+                ForEach(cards) { card in
+                    CardGalleryItem(backgroundStyle: Color.cardFront) {
+                        selectCard(card)
+                    } label: {
+                        Text(card.front)
+                    }
+                }
+            }
+        }
+        .scrollClipDisabled()
+        .navigationDestination(for: Card.self) { selectedCard in
+            CardCarousel(cards: cards, selectedCard: selectedCard)
+        }
+    }
+}
+
+#Preview {
+    CardGallery(cards: Example.contents) { _ in
+        
+    }
+}
