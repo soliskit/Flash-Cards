cf6a24b102734aea09482628e32700eb04ded0b1
David Solis <doze.inept.0n@icloud.com>
2024-03-14 15:47:53 -0700

    add card carousel

---
+++ Flash Cards/CardCarousel.swift
@@ -0,0 +1,41 @@
+//
+//  CardCarousel.swift
+//  Flash Cards
+//
+//  Created by David Solis on 3/14/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct CardCarousel: View {
+    @State private var selectedCardID: PersistentIdentifier?
+    @FocusState private var focusCardID: PersistentIdentifier?
+    private let initialCardID: PersistentIdentifier
+    
+    var cards: [Card]
+    
+    init(cards: [Card], selectedCard: Card) {
+        self.cards = cards
+        initialCardID = selectedCard.id
+    }
+    
+    var body: some View {
+        VStack {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(cards) { card in
+                        Group {
+                            FlashCardView(card: card)
+                                .id(card.id)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CardCarousel(cards: Example.contents, selectedCard: Example.contents[0])
+}
